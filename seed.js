4efcf203c3698a8698c74c73bf276b5c5baba3cb
yusezmann <caritaku.inc@gmail.com>
2022-01-06 13:18:27 +0700

    add images

--- seed.js
+++ seed.js
@@ -503,14 +503,14 @@ var data = [
         nameBank: 'Mandiri',
         nomorRekening: '089898',
         name: 'elfin',
-        imageUrl: 'images/9999999999.png'
+        imageUrl: 'images/logo mandiri.png'
       },
       {
         _id: mongoose.Types.ObjectId('5e96cbe292b97300fc903323'),
         nameBank: 'BCA',
         nomorRekening: '878678',
         name: 'elfin',
-        imageUrl: 'images/9999999999.png'
+        imageUrl: 'images/logo bca.png'
       }
     ]
   },
