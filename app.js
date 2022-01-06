8720a273ad401a2fdfbe0033f5c61e2d3ccfa030
yusezmann <caritaku.inc@gmail.com>
2022-01-06 13:56:30 +0700

    add header

--- app.js
+++ app.js
@@ -51,6 +51,12 @@ app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
